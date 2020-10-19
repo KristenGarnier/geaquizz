985504262b4db4465199344b1c57c2abee6c029f
Kristen Garnier <kristen@nerdy-bear.com>
2020-10-19 17:13:39 +0200

    update app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import quizQuestions from './api/quizQuestions';
 import Quiz from './components/Quiz';
 import Result from './components/Result';
